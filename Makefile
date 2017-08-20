de972b058f97951d1405bd6bbf4e0c3118130420
Steven Smith <sos22@srcf.ucam.org>
2017-08-19 20:56:43 -0700

    Auto-generate some standard operators as well.
    Suspect this'll work badly with virtual inheritance.

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
-lsmt: lsmt.C
-	clang++ -std=gnu++1y -Wall -g lsmt.C -o lsmt
+lsmt: lsmt.C meta.H testops.o
+	clang++ -O -std=gnu++1y -Wall -g lsmt.C testops.o -o lsmt
+
+testops.o: testops.C meta.H
+	clang++ -O3 -std=gnu++1y -Wall -c -g testops.C 
+
---
+++ testops.C
@@ -0,0 +1,54 @@
+#include "meta.H"
+
+#include "testops.H"
+
+struct withopers : meta<withopers> {
+    structA a;
+    structB b;
+    withopers(int _a, int _b) : a(_a), b(_b) {}
+    template <typename visitor> bool visit(visitor && v) {
+        return v("a", a) && v("b", b); } };
+
+void dotest() {
+    for (unsigned a = 0; a < 3; a++) {
+        for (unsigned b = 0; b < 3; b++) {
+            withopers x(a, b);
+            for (unsigned c = 0; c < 3; c++) {
+                for (unsigned d = 0; d < 3; d++) {
+                    withopers y(c, d);
+                    if (a < c) {
+                        assert(x <= y);
+                        assert(x < y);
+                        assert(!(x == y));
+                        assert(x != y);
+                        assert(!(x > y));
+                        assert(!(x >= y)); }
+                    else if (a > c) {
+                        assert(!(x <= y));
+                        assert(!(x < y));
+                        assert(!(x == y));
+                        assert(x != y);
+                        assert(x > y);
+                        assert(x >= y); }
+                    else {
+                        if (b < d) {
+                            assert(x <= y);
+                            assert(x < y);
+                            assert(!(x == y));
+                            assert(x != y);
+                            assert(!(x > y));
+                            assert(!(x >= y)); }
+                        else if (b == d) {
+                            assert(x <= y);
+                            assert(!(x < y));
+                            assert(x == y);
+                            assert(!(x != y));
+                            assert(!(x > y));
+                            assert(x >= y); }
+                        else {
+                            assert(!(x <= y));
+                            assert(!(x < y));
+                            assert(!(x == y));
+                            assert(x != y);
+                            assert(x > y);
+                            assert(x >= y); } } } } } } }
